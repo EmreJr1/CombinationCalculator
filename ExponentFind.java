7cc021c4055d3ea57e865541ac459ab62031abea
Emre <124446702+EmreJr1@users.noreply.github.com>
2023-05-17 20:39:49 +0300

    Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.

--- ExponentFind.java
+++ ExponentFind.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ExponentFind {
 
 
+       //4 ve 5 in kuvvetlerini bulan program
 
-}
+        public static void main(String[] args) {
+            int userInput ;
+
+
+            //kullanıcı veri girişi
+            Scanner inp = new Scanner(System.in);
+            System.out.print("Sayı giriniz =");
+            userInput=inp.nextInt();
+
+
+            //4 ün katlarını bulma
+            for (var i=1; i<=userInput;i*=4){
+                System.out.println(i);
+            }
+            System.out.println("*************************");
+
+            //5 in katlarını bulma
+            for (var i=1; i<=userInput;i*=5) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+
